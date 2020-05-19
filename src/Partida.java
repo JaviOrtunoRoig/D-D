49693b5a02537eeb4aeb0baebc198c0168f540d3
Javier Ortuño Roig <javi_roig@hotmail.es>
2020-05-19 12:40:05 +0200

    subida totoal de archivos

---
+++ src/Partida.java
@@ -0,0 +1,19 @@
+public class Partida {
+
+    boolean metidoRegistro;
+    boolean metidoInicioSesion;
+
+    public Partida() {
+        metidoInicioSesion = false;
+        metidoRegistro = false;
+    }
+
+    public boolean getMetidoRegistro() {return metidoRegistro;}
+
+    public boolean getMetidoInicioSesion() {return metidoInicioSesion;}
+
+    public boolean setMetidoRegistro() {return metidoRegistro;}
+
+    public boolean setMetidoInicioSesion() {return metidoInicioSesion;}
+
+}
