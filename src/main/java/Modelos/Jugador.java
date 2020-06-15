4dfb52b2b512214888e08c1394955967b542093c
DCasber <davidcastillo@uma.es>
2020-06-15 11:45:27 +0200

    ListaJugadores+Jugador

--- src/main/java/Modelos/Jugador.java
+++ src/main/java/Modelos/Jugador.java
@@ -2,4 +2,32 @@ package Modelos;
 
 public class Jugador {
 
+    private String nombreUsuario;
+    private String nombrePersonaje;
+    private int vida;
+
+    public Jugador(String Usuario, String Personaje, int vida)
+    {
+        nombreUsuario = Usuario;
+        nombrePersonaje = Personaje;
+        this.vida = vida;
+    }
+
+    public String getNombreUsuario()
+    {
+        return nombreUsuario;
+    }
+
+    public String getNombrePersonaje()
+    {
+        return nombrePersonaje;
+    }
+
+    public int getVida()
+    {
+        return vida;
+    }
+
+
+
 }
