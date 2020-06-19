be1d09bafb4f705812941d5afa4f420553469b41
jesu-m0 <jesusmd2000@gmail.com>
2020-06-19 19:48:27 +0200

    UpdateVida y arreglos

--- src/main/java/metodosBDD/ModificarStats.java
+++ src/main/java/metodosBDD/ModificarStats.java
@@ -20,15 +20,12 @@ public class ModificarStats {
 
 
     /**
-     *
-     * @param idPersonaje
-     * @return
-     * int:
+     * @param Usuario
+     * @return int:
      * vida personaje
      * -1 si no existe
      */
-    public int getVida  (int idPersonaje) throws SQLException, ClassNotFoundException {
-
+    public int getVida(String Usuario) throws SQLException, ClassNotFoundException {
 
 
         Class.forName(JDBC_DRIVER);
@@ -37,11 +34,63 @@ public class ModificarStats {
         int vida = -1;
 
 
-        String sqlConsulta = "SELECT vida FROM Personaje WHERE idPersonaje = " + idPersonaje + ";";
-        ResultSet resultado=stmt.executeQuery(sqlConsulta);
+        String sqlConsulta = "SELECT vida FROM Personaje WHERE Usuario = '" + Usuario + "';";
+        ResultSet resultado = stmt.executeQuery(sqlConsulta);
         resultado.next();
-        vida=resultado.getInt("vida");
+        vida = resultado.getInt("vida");
         return vida;
+
+    }
+
+
+    /**
+     * @param Usuario
+     * @param newVida
+     * @return 1 si bien, 2 si no existe el personaje-usuario, 3 error de conexión con la BDD
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public int modificarVida(String Usuario, int newVida) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://database-iis.cobadwnzalab.eu-central-1.rds.amazonaws.com/dungeonsdragonsdb", "dundragons", "VengerHank");
+        stmt = conn.createStatement();
+
+        if(existeUsuario(Usuario)==1){
+            String sqlConsulta = "UPDATE `dungeonsdragonsdb`.`Personaje` SET `vida` = '"
+                    + newVida + "' WHERE (`Usuario` = '"
+                    + Usuario + "');";
+            stmt.executeUpdate(sqlConsulta);
+            System.out.println("Sentencia ejecutada con exito.");
+            return 1;
+        }else{
+            return existeUsuario(Usuario);
+        }
+
+
+    }
+
+
+
+    public int getExperiencia(String Usuario) throws SQLException, ClassNotFoundException {
+
+
+        Class.forName(JDBC_DRIVER);
+        conn = DriverManager.getConnection(DB_URL + "/" + DB_SCHEMA, USER, PASS);
+        stmt = conn.createStatement();
+        int vida = -1;
+
+
+        if(existeUsuario(Usuario)==1){
+            String sqlConsulta = "SELECT experiencia FROM Personaje WHERE Usuario = '" + Usuario + "';";
+            ResultSet resultado = stmt.executeQuery(sqlConsulta);
+            resultado.next();
+            vida = resultado.getInt("vida");
+            return vida;
+        }else{
+            return existeUsuario(Usuario);
+        }
+
 
     }
 
@@ -49,4 +98,69 @@ public class ModificarStats {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     *
+     * @param Usuario
+     * @return
+     * 1 existe
+     * 2 error bdd
+     * 3 error conexion
+     */
+    private int existeUsuario(String Usuario) {
+
+        Connection conn = null;
+        Statement stmt = null;
+        String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://database-iis.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+        String DB_SCHEMA = "dungeonsdragonsdb";
+        String USER = "dundragons";
+        String var8 = "VengerHank";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://database-iis.cobadwnzalab.eu-central-1.rds.amazonaws.com/dungeonsdragonsdb", "dundragons", "VengerHank");
+            stmt = conn.createStatement();
+        } catch (ClassNotFoundException e) {
+            return 3;
+        } catch (SQLException e) {
+            return 3;
+        }
+
+        ResultSet resultado;
+        try {
+            String sqlConsulta = "SELECT Nombre FROM Personaje WHERE Usuario = '" + Usuario + "';";
+            resultado = stmt.executeQuery(sqlConsulta);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return 2;
+        }
+
+        try {
+            if (resultado.getRow() == 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } catch (SQLException e) {
+            return 2;
+        }
+    }
 }
